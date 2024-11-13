1b26142833157719d7a010074ba6a63e206914fe
Sudharsan <sudharsan.23bcs10077@ms.sst.scaler.com>
2024-11-13 22:51:55 +0530

    feat(analytics): add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { NuqsAdapter } from "nuqs/adapters/next";
 
 export const metadata = {
@@ -16,8 +17,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <GoogleTagManager gtmId="GTM-597LXR5M" />
       </head>
       <body className="vsc-initialized flex min-h-screen flex-col text-teal-950 antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-597LXR5M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <NuqsAdapter>{children}</NuqsAdapter>
       </body>
     </html>
